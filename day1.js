6369620d290477d532fa715c3b57cfca7d7d75a7
Shivanandi <115460687+Shivanandi@users.noreply.github.com>
2022-10-10 15:11:37 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,59 @@
+//  var a = document
+// var a="Nandini";
+// var a1=fkvijdifjl;
+// var b=1;
+// var c=true;
+// var d="w";
+// var e=null;
+// var f=undefined;
+
+// var g =1;
+// var h=2;
+// function sum(g,b){
+//     let jk="jdsnjls";
+//     return g+b;
+// }
+// console.log(sum(g,b));
+
+// let i=4;
+// const t="gf";
+
+// let jh=0;
+// if(jh===0){
+//     console.log("true");
+// }else{
+//     console.log("false"); 
+// }
+// for(i=0;i<=5;i++){
+//     // jh = jh + 1;
+//     // console.log(jh);
+//     console.log(i);
+// }
+// let array=[1,"nandini",true];
+// let obj={
+//     "name":"nan",
+//     "number":1,
+//     "numbers":2,
+//     "names":"ravi"
+// };
+// var k = carrot;
+// var m="Nandini";
+// function f1(){
+//     // window.alert("f1() is called");
+// 	var x = document.getElementById("num1").value;
+// 	var y = document.getElementById("num2").value;
+//     // var sum = x+y;
+// 	var sum = parseInt(x)+parseInt(y);
+//     // console.log(sum);
+//     document.getElementById("sp1").innerText=sum;
+
+// }
+function d1(){
+    window.alert("d1() is called")
+    var x = document.getElementById("num1").value;
+    var y = document.getElementById("num2").value;
+    var sum = x+y;
+    var sum = parseInt(x)+parseInt(y);
+    console.log(sum);
+    document.getElementById("done1").innerText=sum;
+}
